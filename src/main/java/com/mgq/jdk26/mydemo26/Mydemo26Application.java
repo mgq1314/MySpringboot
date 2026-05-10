8bc9affac6d48ce893102ad8037babbe1d3be83f
mgq <1435539309@qq.com>
2026-05-10 19:28:15 +0800

    444

---
+++ src/main/java/com/mgq/jdk26/mydemo26/Mydemo26Application.java
@@ -0,0 +1,13 @@
+package com.mgq.jdk26.mydemo26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mydemo26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mydemo26Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mgq/jdk26/mydemo26/Mydemo26ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgq.jdk26.mydemo26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mydemo26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
